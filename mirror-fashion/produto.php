7f0e8ac07f65861b0f4bf8179cb8e49f32517be5
jezzipan <jessica.leute@gmail.com>
2017-07-06 11:28:08 -0300

    adcionado formularia à page uso de php e html

--- mirror-fashion/produto.php
+++ mirror-fashion/produto.php
@@ -8,14 +8,37 @@
    <link rel="stylesheet" href="css/reset.css">  
    <link rel="stylesheet" href="css/estilo.css">
    <meta name="viewport" content="width=device-width">
-   <link rel="stylesheet" href="css/mobile.css" media="(max-width: 320px)">
+   <link rel="stylesheet" href="css/mobile.css" media="(max-width: 320px">
  </head>
     <body>
-        <?php include ("cabecalho.php"); ?>
-        <!-- Aqui vai ... -->
+    <?php include("cabecalho.php"); ?>
+        
+    <form>
+      <fieldset>    
+        <legend> Escolha a cor: </legend>
 
-        <?php include ("rodape.php"); ?>
+        <input type="radio" name="cor" value="verde" id="verde" checked>
+        <label for="verde">
+            <img src="img/produtos/foto2-verde.png" alt="verde">
+        </label>
 
+
+        <input type="radio" name="cor" value="rosa" id="rosa" checked>
+        <label for="rosa">
+            <img src="img/produtos/foto2-rosa.png" alt="rosa">
+        </label>
+
+
+        <input type="radio" name="cor" value="azul" id="azul" checked>
+        <label for="azul">
+            <img src="img/produtos/foto2-verde.png" alt="azul">
+        </label>
+      </fieldset>
+      <input type="submit" class="comprar" value="Comprar">
+    </form>
+
+    <?php include("rodape.php"); ?>
+        
 
     </body>
 <html>
